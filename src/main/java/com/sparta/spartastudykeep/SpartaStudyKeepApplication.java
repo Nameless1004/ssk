b21fb82a162c10f53e5cbab3b82b1d6ebd58207c
Nameless1004 <jjho1029@gmail.com>
2024-09-02 13:59:38 +0900

    프로젝트 생성

---
+++ src/main/java/com/sparta/spartastudykeep/SpartaStudyKeepApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.spartastudykeep;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpartaStudyKeepApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpartaStudyKeepApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sparta/spartastudykeep/SpartaStudyKeepApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.spartastudykeep;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpartaStudyKeepApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
