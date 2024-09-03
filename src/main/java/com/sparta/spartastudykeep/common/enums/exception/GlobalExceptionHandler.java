1cc6d657ae58fb49f3000798da2b3c16649fcb24
Nameless1004 <jjho1029@gmail.com>
2024-09-03 23:35:37 +0900

    [feat] 전역예외처리 클래스 추가 * RestControllerAdvice

---
+++ src/main/java/com/sparta/spartastudykeep/common/enums/exception/GlobalExceptionHandler.java
@@ -0,0 +1,52 @@
+package com.sparta.spartastudykeep.common.enums.exception;
+
+import jakarta.validation.ConstraintViolationException;
+import java.util.HashMap;
+import java.util.Map;
+import org.springframework.dao.DataAccessException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler({IllegalArgumentException.class,
+        IllegalStateException.class, DataAccessException.class})
+    public ResponseEntity<String> handleIllegalArgumentException(RuntimeException ex) {
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler({MethodArgumentNotValidException.class})
+    public ResponseEntity<Map<String, String>> handleValidationExceptions(
+        MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult()
+            .getAllErrors()
+            .forEach(c -> errors.put(((FieldError) c).getField(), c.getDefaultMessage()));
+        return ResponseEntity.badRequest()
+            .body(errors);
+    }
+
+    @ExceptionHandler({ConstraintViolationException.class})
+    ResponseEntity<Map<String, String>> onConstraintValidationException(
+        ConstraintViolationException e) {
+        var constraintViolations = e.getConstraintViolations();
+        Map<String, String> errors = new HashMap<>();
+        for (final var constraint : constraintViolations) {
+
+            String message = constraint.getMessage();
+            String[] split = constraint.getPropertyPath()
+                .toString()
+                .split("\\.");
+            String propertyPath = split[split.length - 1];
+            errors.put(propertyPath, message);
+
+        }
+        return ResponseEntity.badRequest()
+            .body(errors);
+    }
+}
