107af8c44efb8a6e7177189155a467a604bdd86b
Nameless1004 <jjho1029@gmail.com>
2024-09-04 04:58:27 +0900

    [refactor] 안쓰이는 import 제거

--- src/main/java/com/sparta/spartastudykeep/repository/BoardRepository.java
+++ src/main/java/com/sparta/spartastudykeep/repository/BoardRepository.java
@@ -1,14 +1,10 @@
 package com.sparta.spartastudykeep.repository;
 
 import com.sparta.spartastudykeep.entity.Board;
-import com.sparta.spartastudykeep.entity.User;
+import java.util.List;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.web.PageableDefault;
-
-import java.util.List;
 
 public interface BoardRepository extends JpaRepository<Board, Long> {
 
--- src/main/java/com/sparta/spartastudykeep/repository/BookmarkRepository.java
+++ src/main/java/com/sparta/spartastudykeep/repository/BookmarkRepository.java
@@ -1,9 +1,7 @@
 package com.sparta.spartastudykeep.repository;
 
-import com.sparta.spartastudykeep.entity.Board;
 import com.sparta.spartastudykeep.entity.Bookmark;
 import com.sparta.spartastudykeep.entity.User;
-import java.util.List;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
